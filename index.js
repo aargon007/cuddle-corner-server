92474bbaf927c0297c9bbfac530e64540fd42133
md muhaiminul <muhai4959@gmail.com>
2023-05-20 08:44:10 +0600

    get all toys from db and site deploy to vercel

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -13,57 +13,64 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    }
+	serverApi: {
+		version: ServerApiVersion.v1,
+		strict: true,
+		deprecationErrors: true,
+	},
 });
 
 async function run() {
-    try {
-        // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+	try {
+		// Connect the client to the server	(optional starting in v4.7)
+		client.connect();
 
-        const toyCollection = client.db('toyDB').collection('allToys');
+		const toyCollection = client.db("toyDB").collection("allToys");
 
-        //add toy to db
-        app.post("/add-toy", async (req, res) => {
-            //get details from client
-            const body = req.body;
-            body.createdAt = new Date();
-            console.log(body);
-            //post to dbcollection
-            const result = await toyCollection.insertOne(body);
+		//get all toys from db
+		app.get("/all-toys", async (req, res) => {
+			const toys = await toyCollection
+				.find({})
+				.sort({ createdAt: -1 })
+				.toArray();
+			res.send(toys);
+		});
 
-            if (result?.insertedId) {
-              return res.status(200).send(result);
-            } else {
-              return res.status(404).send({
-                message: "can not post toy details, try again leter",
-                status: false,
-              });
-            }
-          });
+		//add toy to db
+		app.post("/add-toy", async (req, res) => {
+			//get details from client
+			const body = req.body;
+			body.createdAt = new Date();
+			console.log(body);
+			//post to dbcollection
+			const result = await toyCollection.insertOne(body);
 
+			if (result?.insertedId) {
+				return res.status(200).send(result);
+			} else {
+				return res.status(404).send({
+					message: "can not post toy details, try again leter",
+					status: false,
+				});
+			}
+		});
 
-          
-        // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-        // Ensures that the client will close when you finish/error
-        // await client.close();
-    }
+		// Send a ping to confirm a successful connection
+		await client.db("admin").command({ ping: 1 });
+		console.log(
+			"Pinged your deployment. You successfully connected to MongoDB!"
+		);
+	} finally {
+		// Ensures that the client will close when you finish/error
+		// await client.close();
+	}
 }
 run().catch(console.dir);
 
-
-
-app.get('/', (req, res) => {
-    res.send('server is running')
-})
+app.get("/", (req, res) => {
+	res.send("server is running");
+});
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
-})
+	console.log(`Server is running on port ${port}`);
+});
